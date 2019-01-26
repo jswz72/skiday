e8bf49a4c82b3baa0ba58f2bde9facd2e21bdc52
jswz72 <jacobdasword@gmail.com>
2019-01-26 11:03:04 -0500

    reorg dirs, populate inital values of db in init, write to conditoins and metrics tables

--- skiday/definitions.py
+++ skiday/definitions.py
@@ -3,3 +3,10 @@ import os
 _ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 DATA_PATH = os.path.join(_ROOT_DIR, 'data/')
 DB_PATH = os.path.join(DATA_PATH, 'data.db')
+
+RESORTS = [{
+    'name': 'cannon',
+    'display': 'Cannon Mountain',
+    'webpage': 'https://www.cannonmt.com/mountain-report'
+    }]
+METRICS = ['hours', 'snow', 'trails', 'lifts']
--- skiday/init_db.py
+++ skiday/init_db.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-from definitions import DATA_PATH
+from definitions import DATA_PATH, RESORTS, METRICS
 
 print(DATA_PATH)
 if not os.path.exists(DATA_PATH):
@@ -10,8 +10,18 @@ if not os.path.exists(DATA_PATH):
 db = sqlite3.connect('../data/data.db')
 cursor = db.cursor()
 cursor.execute('CREATE TABLE resorts(id integer primary key, name varchar(255), display varchar(255));')
-cursor.execute('CREATE TABLE conditions(resort integer not null, description varchar(255), date text, foreign key(resort) references resorts(id));')
+cursor.execute('CREATE TABLE conditions(resort integer not null, description varchar(255), date varchar(255), foreign key(resort) references resorts(id));')
 cursor.execute('CREATE TABLE metric_types(id integer not null primary key, type varchar(255));')
-cursor.execute('CREATE TABLE metrics(resort integer not null, metric integer not null, date text, data integer, foreign key(resort) references resorts(id), foreign key(metric) references metric_types(id));')
+cursor.execute('CREATE TABLE metrics(resort integer not null, metric integer not null, date varchar(255), data varchar(255), foreign key(resort) references resorts(id), foreign key(metric) references metric_types(id));')
 cursor.execute('CREATE TABLE webpages(resort integer not null, url varchar(255), foreign key(resort) references resorts(id))')
+
+for resort in RESORTS:
+    cursor.execute('INSERT INTO resorts (name, display) VALUES (?, ?)', (resort['name'], resort['display']))
+    cursor.execute('SELECT id from resorts where name=?', (resort['name'],))
+    rid = int(cursor.fetchone()[0])
+    cursor.execute('INSERT INTO webpages VALUES (?, ?)', (rid, resort['webpage']))
+
+for metric in METRICS:
+    cursor.execute('INSERT INTO metric_types (type) VALUES (?)', (metric,))
+
 db.commit()
